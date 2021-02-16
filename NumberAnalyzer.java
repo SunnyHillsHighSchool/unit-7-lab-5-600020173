c0b7b9607ea14a4726a92cc860473e6670086ef6
Jonathan Hsing <600020173@fjuhsd.org>
2021-02-16 15:17:04 +0000

    added NumberAnalyzer

--- NumberAnalyzer.java
+++ NumberAnalyzer.java
@@ -11,14 +11,58 @@ public class NumberAnalyzer
 	//write a constructor
   public NumberAnalyzer(int[]n)
   {
+    list = new ArrayList<Number>();
     for(int a: n)
     list.add(new Number(a));
   }
 	//write count odds
-
+  public int countOdds(){
+    //create a variable
+    int oddCount = 0;
+    //for loop to loop through each element
+    for(Number i:list){
+      //If the number is odd
+      if(i.isOdd()){
+        //add 1
+        oddCount++;
+      }
+    }
+    //return variable
+    return oddCount;
+  }
+  
 	//write count evens
-
+  public int countEvens(){
+    //create a variable
+    int evenCount = 0;
+    //for loop to loop through each element
+    for(Number i:list){
+      //if the number is even
+      if(!i.isOdd()){
+        //add 1
+        evenCount++;
+      }
+    }
+    //return variable
+    return evenCount;
+  }
 	//write count perfects
-	
+	public int countPerfects(){
+    //create variable
+    int perfectCount = 0;
+    //for loop to loop through each element
+    for(Number i:list){
+      //If the number is perfect
+      if(i.isPerfect()){
+        //add 1
+        perfectCount++;
+      }
+    }
+    //return variable
+    return perfectCount;
+  }
 	//write a toString
+  public String toString(){
+    return "" + list;
+  }
 }
